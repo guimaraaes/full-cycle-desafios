2284ca01f2da3cf14392e110cd961be436c5bf68
Sara Guimarães <44955079+guimaraaes@users.noreply.github.com>
2024-07-22 17:47:49 -0300

    add teste para dois erros em product

--- fc-clean-architecture-main/src/domain/product/entity/product.spec.ts
+++ fc-clean-architecture-main/src/domain/product/entity/product.spec.ts
@@ -19,6 +19,12 @@ describe("Product unit tests", () => {
     }).toThrowError("price must be greater than 0");
   });
 
+  it("should throw error when name is empty and price is less than zero", () => {
+    expect(() => {
+      const product = new Product("123", "", -1);
+    }).toThrowError("product: Name is required,product: price must be greater than 0");
+  });
+
   it("should change name", () => {
     const product = new Product("123", "Product 1", 100);
     product.changeName("Product 2");
